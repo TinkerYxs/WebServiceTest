192704e16977e27e428ce27f73a0772dfab491f3
ThinkerYxs <dashuai19910812@163.com>
2018-10-12 11:55:51 +0800

    练习使用axis开发WebService

--- axisWebService/src/com/ll/server/HelloWorld.java
+++ axisWebService/src/com/ll/server/HelloWorld.java
@@ -2,7 +2,9 @@ package com.ll.server;
  
 public class HelloWorld {
     public String sayHello(String username){ 
-        return spake()+"hello, " + username; 
+		PerInfo perInfo = new PerInfo();
+        perInfo.setName("元晓帅");
+		return spake() + "hello, " + username + perInfo.name;
     } 
     public String spake(){
         return "调用了HelloWorld类的spake方法";
---
+++ axisWebService/src/com/ll/server/PerInfo.java
@@ -0,0 +1,52 @@
+/**  
+ * All rights Reserved, Designed By YuanXiaoShuai
+ * @Title:  PerInfo.java   
+ * @Package com.ll.server   
+ * @Description:    TODO(用一句话描述该文件做什么)   
+ * @author: YuanXiaoShuai  
+ * @date:   2018年10月12日 上午11:46:11   
+ * @version: 
+ * @Copyright:
+ */
+package com.ll.server;
+
+/**   
+ * @ClassName:  PerInfo   
+ * @Description:TODO(这里用一句话描述这个类的作用)   
+ * @author: YuanXiaoShuai 
+ * @date:   2018年10月12日 上午11:46:11   
+ *     
+ * @Copyright:  
+ */
+public class PerInfo {
+
+	/**   
+	 * @Title:  PerInfo   
+	 * @Description:    TODO(这里用一句话描述这个方法的作用)   
+	 * @param:    
+	 * @throws   
+	 */
+	public String name;
+
+	/**
+	 * @Title: getName <BR>
+	 * @Description: please write your description <BR>
+	 * @return: String <BR>
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @Title: setName
+	 * @Description: please write your description
+	 * @return: String
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	public PerInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
